51fecc29ed589b7ec605f6aee88da9de84a3730a
李苗雨 <sharonlee218@yahoo.com.cn>
2017-04-05 13:14:29 -0700

    Initial Commit

---
+++ TrafficLight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TrafficLight
+//
+//  Created by Miaoyu li on 4/5/17.
+//  Copyright © 2017 Miaoyu li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TrafficLight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TrafficLight
+//
+//  Created by Miaoyu li on 4/5/17.
+//  Copyright © 2017 Miaoyu li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
