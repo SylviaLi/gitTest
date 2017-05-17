2c95d61744be74f488705506e1828cf27b0a091d
Miaoyu li <miaoyuli@Miaoyus-MacBook-Pro.local>
2017-05-17 15:46:37 -0700

    hello world

--- TrafficLight/ViewController.h
+++ TrafficLight/ViewController.h
@@ -8,8 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    NSTimer *timer;
+    NSTimer *scoreTimer;
+    
+    int timerInt;
+    int scoreInt;
+}
 
+@property (strong, nonatomic) IBOutlet UIImageView *trafficLight;
+@property (strong, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (strong, nonatomic) IBOutlet UIButton *startButton;
+
+- (IBAction)startStop:(id)sender;
 
 @end
 
